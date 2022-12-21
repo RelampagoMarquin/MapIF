17caa1e7d6302fdf689f9ee026fc32a9a74d3a5d
Francisca Natália Simões <francisca.araujo@imobme.com>
2022-12-21 19:32:13 -0300

    Implementando a função de pegar a localização atual

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,44 +1,18 @@
 <script setup>
-import { ref } from "vue";
-import { onMounted } from "@vue/runtime-core";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-
-const mapElement = ref(null);
-
-onMounted(() => {
-  var map = L.map(mapElement.value).setView([-6.25309, -36.53401], 19);
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 21, //fica bugado com mais que 19
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
-
-  //fazer uma marcação e adicona um texto
-  L.marker([-6.25309, -36.53401])
-    .addTo(map)
-    .bindPopup("Aqui é bom pra namorar.<br> Traga seu amor.")
-    .openPopup();
-
-  //criar um polygono
-  var polygon = L.polygon([
-    [-6.253142585162962, -36.53404412963343],
-    [-6.2531609012307, -36.53403152257928],
-    [-6.253168131256544, -36.53404364474693],
-    [-6.253151743196412, -36.53405406981031],
-    [-6.253145236172287, -36.534043402303155],
-    [-6.2531442721691235, -36.534041220313384],
-  ])
-    .addTo(map)
-    .bindPopup("I am a polygon.");
-});
+import Map from "../components/Map.vue";
 </script>
 
 <template>
   <div class="main">
-    <div id="map" ref="mapElement"></div>
+    <Map></Map>
 
-    <v-bottom-navigation grow absolute bg-color="#389c37" color="#000000">
+    <v-bottom-navigation
+      grow
+      absolute
+      bg-color="#389c37"
+      color="#000000"
+      class="bottom-navigation"
+    >
       <v-btn value="recent">
         <v-icon>mdi-calendar-plus</v-icon>
 
@@ -67,11 +41,6 @@ onMounted(() => {
 </template>
 
 <style scoped>
-.main {
-  position: relative;
-  padding: 0;
-  margin: 0;
-}
 .v-icon {
   color: white;
 }
@@ -82,8 +51,8 @@ onMounted(() => {
 .text-white {
   color: white;
 }
-#map {
-  height: 100vh;
-  width: 100vw;
+
+.v-bottom-navigation {
+  position: absolute;
 }
 </style>
