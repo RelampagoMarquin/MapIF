cff1048618b3b3b1d5aec0d9ec2b86aa22391ac5
nataliasimoes <natlia932@gmail.com>
2023-02-01 22:28:55 -0300

    Add snackbar no createEvent

--- web/src/views/CreateEvent.vue
+++ web/src/views/CreateEvent.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import {useEventStore} from '../stores/eventStore'
-const eventStore = useEventStore()
-const nome = ref('')
-const dataInicio = ref('')
-const dataFim = ref('')
-const descricao = ref('')
+import { useEventStore } from "../stores/eventStore";
+const eventStore = useEventStore();
+const nome = ref("");
+const dataInicio = ref("");
+const dataFim = ref("");
+const descricao = ref("");
+let snackbarSucess = ref(false);
+let snackbarFailed = ref(false);
 
-function createEvent() {
+function cleanForm() {
+  nome.value = "";
+  dataInicio.value = "";
+  dataFim.value = "";
+  descricao.value = "";
+}
+
+async function createEvent() {
   const data = {
     nome: nome.value,
     comeca: new Date(dataInicio.value),
@@ -17,8 +26,13 @@ function createEvent() {
     isPublic: true,
   };
 
-  console.log(data)
-  eventStore.createEvent(data);
+  const createEvent = await eventStore.createEvent(data);
+  if (createEvent) {
+    snackbarSucess.value = true;
+    cleanForm();
+  } else {
+    snackbarFailed.value = true;
+  }
 }
 </script>
 
@@ -61,16 +75,48 @@ function createEvent() {
               class="form-control input-camp rounded-pill elevation-4"
             />
             <label for="descricao" class="mt-3 text-label">Descrição</label>
-            <textarea name="descricao" maxlength="225" id="" cols="30" rows="4" class="form-control input-camp rounded-border elevation-4" v-model="descricao"></textarea>
+            <textarea
+              name="descricao"
+              maxlength="225"
+              id=""
+              cols="30"
+              rows="4"
+              class="form-control input-camp rounded-border elevation-4"
+              v-model="descricao"
+            ></textarea>
           </form>
         </div>
         <div>
-              <v-btn class="btn mt-8 p-4" x-large block rounded="lg" @click="createEvent()">
-                <span class="mr-4">Cadastrar Evento</span>
-              </v-btn>
-            </div>
+          <v-btn
+            class="btn mt-8 p-4"
+            x-large
+            block
+            rounded="lg"
+            @click="createEvent()"
+          >
+            <span class="mr-4">Cadastrar Evento</span>
+          </v-btn>
+        </div>
       </v-col>
     </v-row>
+    <v-sheet class="d-flex flex-column">
+      <v-snackbar
+        :timeout="2000"
+        color="green"
+        elevation="24"
+        v-model="snackbarSucess"
+      >
+        Evento cadastrado com sucesso!
+      </v-snackbar>
+      <v-snackbar
+        :timeout="2000"
+        color="red"
+        elevation="24"
+        v-model="snackbarFailed"
+      >
+        Erro ao cadastrar evento!
+      </v-snackbar>
+    </v-sheet>
   </v-container>
 </template>
 
