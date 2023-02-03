c5851520d238cb64b8f384216f597f9a4c06c8f9
nataliasimoes <natlia932@gmail.com>
2023-02-02 22:15:55 -0300

    Ajeitando app bar

---
+++ web/src/components/AppBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main">
+    <v-bottom-navigation
+      grow
+      absolute
+      bg-color="#389c37"
+      color="#000000"
+      class="bottom-navigation"
+    >
+      <v-btn value="recent" to="/event-list">
+        <v-icon>mdi-calendar-plus</v-icon>
+
+        Eventos
+      </v-btn>
+    </v-bottom-navigation>
+  </div>
+</template>
+
+<style scoped>
+.v-icon {
+  color: white;
+}
+
+.v-btn {
+  color: white;
+}
+.text-white {
+  color: white;
+}
+
+.v-bottom-navigation {
+  position: absolute;
+}
+</style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,58 +1,13 @@
 <script setup>
 import Map from "../components/Map.vue";
+import AppBar from "../components/AppBar.vue";
 </script>
 
 <template>
   <div class="main">
     <Map :getLocation="true"></Map>
-
-    <v-bottom-navigation
-      grow
-      absolute
-      bg-color="#389c37"
-      color="#000000"
-      class="bottom-navigation"
-    >
-      <v-btn value="recent">
-        <v-icon>mdi-calendar-plus</v-icon>
-
-        Recent
-      </v-btn>
-
-      <v-btn value="favorites">
-        <v-icon>mdi-cellphone-marker</v-icon>
-
-        Favorites
-      </v-btn>
-
-      <v-btn value="nearby">
-        <v-icon>mdi-map-marker</v-icon>
-
-        Nearby
-      </v-btn>
-
-      <v-btn value="nearby">
-        <v-icon>mdi-map-marker-path</v-icon>
-
-        Nearby
-      </v-btn>
-    </v-bottom-navigation>
+    <AppBar></AppBar>
   </div>
 </template>
 
-<style scoped>
-.v-icon {
-  color: white;
-}
 
-.v-btn {
-  color: white;
-}
-.text-white {
-  color: white;
-}
-
-.v-bottom-navigation {
-  position: absolute;
-}
-</style>
