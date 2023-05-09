90072cc1b59effd495611959c0df89e9149c15d7
Marcos Moreira <marcos_more@outlook.com>
2023-05-09 17:22:01 -0300

    Correção prisma-test-env

--- api/prisma/prisma-test-environment.ts
+++ api/prisma/prisma-test-environment.ts
@@ -1,16 +1,15 @@
-import type { Config } from '@jest/types';
 import { exec } from 'child_process';
-import dotenv, { config } from 'dotenv';
+import { config } from 'dotenv';
 import NodeEnvironment from 'jest-environment-node';
 import { createConnection } from 'mysql2/promise';
-import util, { promisify } from 'util';
-import crypto from 'crypto';
-import {JestEnvironmentConfig} from '@jest/environment';
+import { promisify } from 'util';
+import { JestEnvironmentConfig } from '@jest/environment';
 
 config({ path: '.env.testing' });
-const execSync = promisify(exec)
+const execSync = promisify(exec);
 
-const prismaBinary = './node_modules/.bin/prisma';
+// Instale o prisma globalmente para não da erro
+const prismaBinary = 'prisma';
 
 export declare type EnvironmentContext = {
   console: Console;
@@ -27,18 +26,18 @@ export default class PrismaTestEnvironment extends NodeEnvironment {
 
     const dbUser = process.env.DATABASE_USER;
     const dbPass = process.env.DATABASE_PASS;
-    const dbName = process.env.DATABASENAME;
+    const dbName = process.env.DATABASE_NAME;
     const dbHost = process.env.DATABASE_HOST;
-    const dbPort = process.env.DATABASE_PORT;
+    const dbPort = Number(process.env.DATABASE_PORT);
 
     this.schema = `test_`;
-    this.connectionString = `mysql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?schema=${this.schema}`
+    this.connectionString = `mysql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?schema=${this.schema}`;
   }
 
   async setup() {
     process.env.DATABASE_URL = this.connectionString;
     this.global.process.env.DATABASE_URL = this.connectionString;
-
+    await execSync(`mysql -u ${process.env.DATABASE_USER} -p${process.env.DATABASE_PASS} -h ${process.env.DATABASE_HOST} -e "CREATE DATABASE IF NOT EXISTS ${this.schema}"`);
     await execSync(`${prismaBinary} migrate deploy`);
 
     return super.setup();
