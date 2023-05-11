ac2157a1bd1c0df654aa7581e731033cf27d85a1
Marcos Moreira <marcos_more@outlook.com>
2023-05-11 18:26:33 -0300

    finalizado teste de integração

--- api/test/grupos.e2e-spec.ts
+++ api/test/grupos.e2e-spec.ts
@@ -3,6 +3,8 @@ import { GruposController } from '../src/grupos/grupos.controller';
 import { GruposService } from '../src/grupos/grupos.service';
 import { PrismaService } from '../src/prisma/prisma.service';
 import { GruposModule } from '../src/grupos/grupos.module';
+import { CreateGrupoDto } from '../src/grupos/dto/create-grupo.dto';
+import { UpdateAtividadeDto } from '../src/atividades/dto/update-atividade.dto';
 
 describe('grupos integração', () => {
     let controller: GruposController;
@@ -38,6 +40,73 @@ describe('grupos integração', () => {
             try {
                 const result = await controller.findOne('90');
             } catch( error){
+                expect(error).toThrowError();
+            }
+        })
+    })
+
+    describe('Create', () => {
+        it('Deve rotorna o grupo criado e o tamanho atualizado do banco', async () => {
+            const dto = new CreateGrupoDto
+            dto.nome = "testeCreate"
+            const result = await controller.create(dto);
+            expect(result.nome).toBe(dto.nome);
+            //deve ser o mesmo id que foi informado no input do controller
+            const lenght = await controller.findAll()
+            expect(lenght.length).toBe(4); 
+        })
+        it('devo retornar um erro pois não foi setado o nome', async () => {
+            const dto = new CreateGrupoDto
+            let error
+            try {
+                await controller.create(dto);
+            } catch(err){
+                function error() {
+                    throw new Error('Usuario não encontrado');
+                  }
+                expect(error).toThrowError();
+            }
+        })
+    })
+
+    describe('update', () => {
+        it('Devo retornar um objeto grupo com o mesmo nome e id que foi informado no teste', async () => {
+            const dto = new UpdateAtividadeDto
+            dto.nome = 'teste de alteracao'
+            const result = await controller.update('1', dto);
+            expect(result.nome).toBe(dto.nome);
+            //deve ser o mesmo id que foi informado no input do controller
+            expect(result.id).toBe(1); 
+        })
+        it('devo retornar um erro pois o id não existe no banco', async () => {
+            const dto = new UpdateAtividadeDto
+            let error
+            try {
+                await controller.update('30', dto);
+            } catch(err){
+                function error() {
+                    throw new Error('Usuario não encontrado');
+                  }
+                expect(error).toThrowError();
+            }
+        })
+    })
+
+    describe('delete', () => {
+        it('Devo retornar um objeto grupo com o mesmo id que foi informado no teste', async () => {
+            const result = await controller.findOne('2');
+            expect(result.nome).toBe('teste2');
+            //deve ser o mesmo id que foi informado no input do controller
+            expect(result.id).toBe(2); 
+        })
+        it('devo retornar um erro pois o id não existe no banco', async () => {
+            let error
+            try {
+                await controller.remove('30');
+            } catch(err){
+                function error() {
+                    throw new Error('Usuario não encontrado');
+                  }
                 expect(error).toThrowError();
             }
         })
